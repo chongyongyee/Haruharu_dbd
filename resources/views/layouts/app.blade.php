760fc33ba776df76a905e9fe17d3b337284f1e98
Yy <Poohyee.chong@gmail.com>
2022-10-19 00:59:34 +0800

    img slider with multiple

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,9 @@
     <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/owl.theme.default.min.css') }}" rel="stylesheet">
 
+     <!-- Exzoom Product Images -->
+     <link href="{{ asset('assets/exzoom/jquery.exzoom.css') }}" rel="stylesheet">
+
     <!-- CSS -->
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
     <!-- Default theme -->
@@ -59,6 +62,7 @@
     </script>
 
     <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/exzoom/jquery.exzoom.js') }}"></script>
 
     @yield('scripts')
 
