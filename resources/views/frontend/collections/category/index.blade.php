580de43b5f7cd487706f513bebf5c630a5ad447a
Yy <Poohyee.chong@gmail.com>
2022-10-18 21:04:16 +0800

    faqs minor changes

--- resources/views/frontend/collections/category/index.blade.php
+++ resources/views/frontend/collections/category/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="py-3 py-md-5 bg-light">
-        <div class="container">
+        <div class="container ">
             <div class="row">
                 <div class="col-md-12">
                     <h4 class="mb-4">Our Categories</h4>
@@ -13,10 +13,10 @@
 
                 @forelse ($categories as $categoryItem)
 
-                <div class="col-6 col-md-3">
+                <div class="col-6 col-md-3 ">
                     <div class="category-card">
                         <a href="{{ url('/collections/'.$categoryItem->categoryId) }}">                          
-                            <div class="category-card-body">
+                            <div class="category-card-body ">
                                 <h5>{{ $categoryItem->category}}</h5>
                             </div>
                         </a>
