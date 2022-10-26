3e6ca52e8706b44f67aeae50248fc093b9d699d1
Yy <Poohyee.chong@gmail.com>
2022-10-26 11:36:21 +0800

    dashboard icon size

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,8 +15,14 @@
 
         <div class="cardDashboard">
           <div class="card-inner">
-              <p class="text-dashboard">ORDERS</p>
-            <span class="material-icons-outlined text-green">add_shopping_cart</span>
+            <div class="row">
+              <div class="col-md-3">
+                <p class="text-dashboard">ORDERS</p>
+              </div>
+              <div class="col-md-4 mt-2">
+              <p class="material-icons-outlined text-green float-end">add_shopping_cart</p>
+              </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$totalOrders}}</span>
         </div>
@@ -24,56 +30,98 @@
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">TODAY ORDERS</p>
-            <span class="material-icons-outlined text-red">shopping_cart</span>
+            <div class="row">
+              <div class="col-md-8">
+                <p class="text-dashboard">TODAY ORDERS</p>
+              </div>
+              <div class="col-md-2">
+                <span class="material-icons-outlined text-red">shopping_cart</span>
+              </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$todayOrder}}</span>
         </div>
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">THIS MONTH ORDERS</p>
-            <span class="material-icons-outlined text-blue">local_mall</span>
+            <div class="row">
+                <div class="col-md-8">
+                  <p class="text-dashboard">THIS MONTH ORDERS</p>
+                </div>
+                <div class="col-md-2 mt-2">
+                  <span class="material-icons-outlined text-blue">local_mall</span>
+                </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$thisMonthOrder}}</span>
         </div>
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">THIS YEAR ORDERS</p>
-            <span class="material-icons-outlined text-orange">shopping_bag</span>
+            <div class="row">
+              <div class="col-md-8">
+                <p class="text-dashboard">THIS YEAR ORDERS</p>
+              </div>
+              <div class="col-md-2 mt-2">
+                <span class="material-icons-outlined text-orange">shopping_bag</span>
+              </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{ $thisYearOrder}}</span>
         </div>
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">PRODUCTS</p>
-            <span class="material-icons-outlined text-blue">inventory_2</span>
+            <div class="row">
+                <div class="col-md-8">
+                  <p class="text-dashboard">PRODUCTS</p>
+                </div>
+                <div class="col-md-2">
+                  <span class="material-icons-outlined text-blue">inventory_2</span>
+                </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$totalProducts}}</span>
         </div>
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">CATEGORIES</p>
-            <span class="material-icons-outlined">category</span>
+            <div class="row">
+              <div class="col-md-8">
+                <p class="text-dashboard">CATEGORIES</p>
+              </div>
+              <div class="col-md-2">
+                <span class="material-icons-outlined">category</span>
+              </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$totalCategory}}</span>
         </div>
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">SALES</p>
-            <span class="material-icons-outlined text-orange">monetization_on</span>
+            <div class="row">
+              <div class="col-md-8">
+                <p class="text-dashboard">SALES</p>
+              </div>
+              <div class="col-md-2">
+                <span class="material-icons-outlined text-orange">monetization_on</span>
+              </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$totalSales}}</span>
         </div>
 
         <div class="cardDashboard">
           <div class="card-inner">
-            <p class="text-dashboard">EXPENSES</p>
-            <span class="material-icons-outlined text-red">local_atm</span>
+            <div class="row">
+              <div class="col-md-8">
+                <p class="text-dashboard">EXPENSES</p>
+              </div>
+              <div class="col-md-2">
+                <span class="material-icons-outlined text-red">local_atm</span>
+              </div>
+            </div>
           </div>
           <span class="text-primary font-weight-bold">{{$totalExpense}}</span>
         </div>
